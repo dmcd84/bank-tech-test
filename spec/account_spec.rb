7aefbf5da93341dd5007072680936fe3edf7e8e7
Dom <dom.mcdonnell@gmail.com>
2017-07-10 15:03:36 +0100

    deposits can be made to increase the account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,11 @@ describe Account do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#deposit' do
+    it 'can recieve a deposit to increase the balance' do
+      subject.deposit(100.50)
+      expect(subject.balance).to eq(100.50)
+    end
+  end
 end
