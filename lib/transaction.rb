912eb22ddd6c78156780397569b99264b190d678
Dom <dom.mcdonnell@gmail.com>
2017-07-10 21:25:41 +0100

    Transactions have a date, amount and balance

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,6 +1,10 @@
 class Transaction
-attr_reader :date
-  def initialize
+
+attr_reader :date, :balance, :amount
+
+  def initialize(amount, balance)
     @date = Time.now
+    @balance = balance
+    @amount = amount
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,7 +4,15 @@ require 'timecop'
 describe Transaction do
   it 'initializes with a date' do
     Timecop.freeze do
-      expect(subject.date).to eq Time.now
+      transaction = Transaction.new(100, 100)
+      expect(transaction.date).to eq Time.now
     end
+  end
+
+  it 'returns the balance' do
+    account = Account.new
+    account.deposit(100)
+    transaction = Transaction.new(100, 100)
+    expect(transaction.balance).to eq 100
   end
 end
